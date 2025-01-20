d2ed6b2597ebf77884f458291a31d5f6618ef75f
Aruzhan38 <241372@astanait.edu.kz>
2025-01-20 09:45:37 +0500

    test

--- src/Aruzhan.java
+++
@@ -1,3 +0,0 @@
-public class Aruzhan {
-    private int test = 0;
-}
---
+++ src/package1/Bookings.java
@@ -0,0 +1,15 @@
+package package1;
+
+import java.util.Date;
+
+public class Bookings {
+    private int booking_id;
+    private int customer_id;
+    private int room_id;
+    private Date check_in_date;
+    private Date check_out_date;
+    private float total_price;
+    private String status;
+
+
+}
