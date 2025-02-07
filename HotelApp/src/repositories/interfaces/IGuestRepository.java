1458540657435757141752004945ddaaacf8db95
RassulAidarbekk <241711@astanait.edu.kz>
2025-02-07 09:55:57 +0500

    Guest rep interface validating existence

--- HotelApp/src/repositories/interfaces/IGuestRepository.java
+++ HotelApp/src/repositories/interfaces/IGuestRepository.java
@@ -4,12 +4,13 @@ import models.Guest;
 
 import java.util.List;
 
-public interface
-IGuestRepository {
+public interface IGuestRepository {
     boolean createGuest(Guest guest);
     Guest getGuestById(int id);
     List<Guest> getAllGuests();
     boolean deleteAllGuests();
     void resetGuestIdSequence();
-
+    boolean isEmailExists(String email);
+    boolean isPhoneNumberExists(String phoneNumber);
 }
+
